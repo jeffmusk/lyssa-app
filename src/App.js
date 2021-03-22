3de6c7f61c7678d78d2fcd24b5a9136b902fb092
Jeff Programacion <jeffprogramacion@gmail.com>
2021-03-22 12:54:05 -0500

    chore: add tailwind to react
    add craco and files config

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Lyssa App</p>
+        <div className="bg-red-500 w-5 h-5"></div>
         <p>Be Jeff G</p>
       </header>
     </div>
