2353887ee3354f14fe1d009ef35c958a9278e921
Jeff Programacion <jeffprogramacion@gmail.com>
2021-03-22 11:59:56 -0500

    add first commit with changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Lyssa App</p>
+        <p>Be Jeff G</p>
       </header>
     </div>
   );
